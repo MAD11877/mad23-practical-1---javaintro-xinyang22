d67c6a48a42def6d5adca04fb324af95bae29b4f
xinyang22 <117617152+xinyang22@users.noreply.github.com>
2023-04-24 13:54:31 +0800

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -1,23 +1,19 @@
-import java.util.Scanner;
+
 
 public class Question4
 {
   public static void main(String[] args)
   {
-    /**
-     * Prints a right angle triangle with *. The base of the triangle depends on the integer given.
-     *    e.g.
-     *     > 5
-     *     *****
-     *     ****
-     *     ***
-     *     **
-     *     *
-     * 
-     * Hint: 2 loops are required. System.out.print(...) to print on single line.
-     */
-     
-    Scanner in = new Scanner(System.in);
+    Console.Write("Enter the base of the triangle: ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = n; i >= 1; i--)
+{
+    for (int j = 1; j <= i; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
     
   }
 }
