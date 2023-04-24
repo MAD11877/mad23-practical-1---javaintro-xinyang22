39d970f97fca68633760c169d8e03c0c8b44922c
xinyang22 <117617152+xinyang22@users.noreply.github.com>
2023-04-24 13:53:39 +0800

    Update Question3.java

--- Question3.java
+++ Question3.java
@@ -4,13 +4,10 @@ public class Question3
 {
   public static void main(String[] args)
   {
-    /**
-     * Reads integer entered by the user, multiplies the integer by itself and print out the result.
-     * 
-     * Hint: in.nextInt() to read integer
-     */
-     
-    Scanner in = new Scanner(System.in);
+    Console.Write("Enter a integer to multiply by itself: ");
+    double integer = Convert.ToDouble(Console.ReadLine());
+    double result = integer * integer;
+    Console.WriteLine("The result is " + result);
     
   }
 }
